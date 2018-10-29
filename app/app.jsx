720b7582a3c11b3d067514580bc60cb3b42fbdea
Stefanie Doll <stefie.doll@googlemail.com>
2018-10-29 21:21:05 +0100

    Added environment variables, changed to new geoip lookup service ipstack

--- app/app.jsx
+++ app/app.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import ReactDOM from 'react-dom';
 import 'aframe';
 import {Entity, Scene} from 'aframe-react';
@@ -9,11 +9,13 @@ import Lights from './components/lights.jsx';
 import GitHub from './components/githubLink.jsx';
 import {Welcome, ConnectionError, APIError, Loading} from './components/messages.jsx';
 
-if (window && !window.EventSource){
+if (window && !window.EventSource) {
   window.EventSource = require('./js/eventsource-polyfill.js');
 }
 
-const ApiPort  = (location.protocol == 'https:') ? 443 : 80;
+const ApiPort = (location.protocol == 'https:')
+  ? 443
+  : 80;
 const CSS = require('./assets/styles/style.styl');
 
 class VRStream extends React.Component {
@@ -24,7 +26,7 @@ class VRStream extends React.Component {
       locations: [],
       storedIPs: [],
       ipCoordinates: [],
-      editTitle:'Wikipedia WebVR Recent Changes Heatmap',
+      editTitle: 'Wikipedia WebVR Recent Changes Heatmap',
       overlay: false,
       globeSrc: false,
       VRMode: false,
@@ -42,69 +44,61 @@ class VRStream extends React.Component {
     this.reconnect = null;
     this.eventsource = null;
   }
-  componentWillMount(){
-    this.setState({
-      overlay: <Welcome startFunction={() => this._fetchEdits(true)} />
-    });
+  componentWillMount() {
+    this.setState({overlay: <Welcome startFunction={() => this._fetchEdits(true)}/>});
   }
-  componentDidMount(){
+  componentDidMount() {
     const aScene = document.querySelector('a-scene'),
-          _this = this;
-    if(aScene.isMobile){
-      this.setState({
-        globeSrc: './app/assets/images/natural-earth-small.jpeg'
-      });
+      _this = this;
+    if (aScene.isMobile) {
+      this.setState({globeSrc: './app/assets/images/natural-earth-small.jpeg'});
     } else {
-      this.setState({
-        globeSrc: './app/assets/images/natural-earth.jpg'
-      });
+      this.setState({globeSrc: './app/assets/images/natural-earth.jpg'});
     }
-    aScene.addEventListener('enter-vr', function(){
-      _this.setState({
-        VRMode: true
-      });
+    aScene.addEventListener('enter-vr', function() {
+      _this.setState({VRMode: true});
     });
-    aScene.addEventListener('exit-vr', function(){
-      _this.setState({
-        VRMode: false
-      });
+    aScene.addEventListener('exit-vr', function() {
+      _this.setState({VRMode: false});
     });
 
     /** visibiliyChange EventListener for iOS webvr-polyfill disconnect bug **/
     const keys = {
-        hidden: "visibilitychange",
-        webkitHidden: "webkitvisibilitychange",
-        mozHidden: "mozvisibilitychange",
-        msHidden: "msvisibilitychange"
+      hidden: "visibilitychange",
+      webkitHidden: "webkitvisibilitychange",
+      mozHidden: "mozvisibilitychange",
+      msHidden: "msvisibilitychange"
     };
     for (let stateKey in keys) {
-        if (stateKey in document) {
-            this.eventKey = stateKey;
-            this.eventName = keys[stateKey];
-            break;
-        }
+      if (stateKey in document) {
+        this.eventKey = stateKey;
+        this.eventName = keys[stateKey];
+        break;
+      }
     }
     document.addEventListener(this.eventName, this._handleTabVisibility.bind(this));
   }
-  componentDidUpdate(){
+  componentDidUpdate() {
     // workaround for the webvr-polyfill which disconnects all open network connections after 30000ms in vr-mode on iOS devices (with iOSWakeLock())
     // reopens stream if stream is disconnected on ios in vr-mode
-    if(this.state.isIos && this.state.VRMode && !this.reconnect){
+    if (this.state.isIos && this.state.VRMode && !this.reconnect) {
       let _this = this;
-      this.reconnect =  setInterval(function() {
-        if(_this.eventsource.readyState == 2) _this._fetchEdits(false);
-      }, 500);
+      this.reconnect = setInterval(function() {
+        if (_this.eventsource.readyState == 2) 
+          _this._fetchEdits(false);
+        }
+      , 500);
     } else if (this.state.isIos && !this.state.VRMode && this.reconnect) {
       // remove eventListener for ios when not in vr-mode
       clearInterval(this.reconnect);
       this.reconnect = null;
     }
   }
-  componentWillUnmount(){
+  componentWillUnmount() {
     // remove visibiliyChange EventListener for iOS webvr-polyfill disconnect bug
     // remove interval for ios-bug on willUnmount.
     document.removeEventListener(this.eventName);
-    if(this.reconnect){
+    if (this.reconnect) {
       clearInterval(this.reconnect);
       this.reconnect = null;
     }
@@ -113,56 +107,58 @@ class VRStream extends React.Component {
     // visibiliyChange EventListener for iOS webvr-polyfill disconnect bug
     // only reconnect on ios if tab/ document is visible, remove interval when tab is the background or minimized.
     const tabIsVisible = !e.target[this.eventKey];
-    this.setState({ tabIsVisible: tabIsVisible });
-    if(this.state.isIos && !tabIsVisible){
+    this.setState({tabIsVisible: tabIsVisible});
+    if (this.state.isIos && !tabIsVisible) {
       clearInterval(this.reconnect);
       this.reconnect = null;
     }
   }
   _fetchEdits(indicator) {
     // connect to Wikipedia API Stream
-    if(indicator) this.setState({ overlay: <Loading /> });
-
+    if (indicator) 
+      this.setState({overlay: <Loading/>});
+    
     this.eventsource = new EventSource("https://stream.wikimedia.org/v2/stream/recentchange");
     this.eventsource.onmessage = (message) => {
       // only add new markers when tab is focused/ visible
-      if(this.state.tabIsVisible) this._handleStreamData(message);
-    };
+      if (this.state.tabIsVisible) 
+        this._handleStreamData(message);
+      };
     this.eventsource.onerror = (error) => {
-      this.setState({
-        overlay: <APIError />
-      });
+      this.setState({overlay: <APIError/>});
     }
-
   }
-  _handleStreamData(message){
+  _handleStreamData(message) {
     const data = JSON.parse(message.data),
-          user = data.user,
-          isAnonymous = this._validateIP(user);
+      user = data.user,
+      isAnonymous = this._validateIP(user);
 
     // Only continue if user is anonymous
     if (isAnonymous) {
       // Set marker attributes according to type of edit
       let color = '#ffd11a', // yellow
-          radius = 5,
-          type = 'flag';
+        radius = 5,
+        type = 'flag';
 
-      if(data.length){
-        let dataLengthOld = data.length.old ? data.length.old : 0;
-        radius = (data.length.new - dataLengthOld)/5;
+      if (data.length) {
+        let dataLengthOld = data.length.old
+          ? data.length.old
+          : 0;
+        radius = (data.length.new - dataLengthOld) / 5;
         color = '#00e600', //green
         type = 'add';
-        if(radius <= 0){
+        if (radius <= 0) {
           color = '#ff0000'; //red
           type = 'delete';
           radius = -radius;
         }
-        if(radius >= 21) radius = 21;
-      }
+        if (radius >= 21) 
+          radius = 21;
+        }
       // check if there are stored coordinates for ip-address
-      if(this.state.ipCoordinates.length){
+      if (this.state.ipCoordinates.length) {
         const userIndex = this.state.storedIPs.indexOf(user);
-        if(userIndex == -1){
+        if (userIndex == -1) {
           // new IP address -> get coordinates with API call
           this._getCoordinates(user, radius, color, type, data.title);
         } else {
@@ -175,91 +171,108 @@ class VRStream extends React.Component {
       }
     }
   }
-  _getCoordinates(user, radius, color, type, title){
+  _getCoordinates(user, radius, color, type, title) {
     const options = {
-      uri: `http://freegeoip.net:${ApiPort}/json/${user}`,
-      headers: {'User-Agent': 'Request-Promise'},
+      //uri: `http://freegeoip.net:${ApiPort}/json/${user}`,
+      uri: `http://api.ipstack.com/${user}?access_key=${process.env.API_KEY}`,
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
       json: true
     };
     // Look up coordinates for IP address of anonymous user
-    Rp(options)
-    .then((location) => {
-        //  Add marker if IP lookup was successful
-        this.setState(previousState => ({
-          storedIPs:  [...previousState.storedIPs, user],
-          ipCoordinates: [...previousState.ipCoordinates, { user, 'coordinates': [location.latitude, location.longitude]}],
-          overlay: false
-        }));
-        this._addLocations(location.latitude, location.longitude, radius, color, type, title);
-    })
-    .catch((err) => {
-      this.setState({
-        overlay: <ConnectionError />
-      });
+    Rp(options).then((location) => {
+      //  Add marker if IP lookup was successful
+      this.setState(previousState => ({
+        storedIPs: [
+          ...previousState.storedIPs,
+          user
+        ],
+        ipCoordinates: [
+          ...previousState.ipCoordinates, {
+            user,
+            'coordinates': [location.latitude, location.longitude]
+          }
+        ],
+        overlay: false
+      }));
+      this._addLocations(location.latitude, location.longitude, radius, color, type, title);
+    }).catch((err) => {
+      this.setState({overlay: <ConnectionError/>});
     });
   }
-  _validateIP(user){
+  _validateIP(user) {
     //  check if user has a valid IP
-     if(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(user)){
-        return (true)
-      }
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(user)) {
+      return (true)
+    }
     return (false)
   }
   _addLocations(lat, lng, radius, color, type, title) {
     // calculate marker position on globe
     const index = this.state.edits,
-          phi = (90-lat) * (Math.PI/180),
-          theta = -((lng+180) * (Math.PI/180)),
-          positionX = -((28 - (index/500)) * Math.sin(phi) * Math.cos(theta)),
-          positionY = ((28 - (index/500)) * Math.cos(phi)),
-          positionZ = ((28 - (index/500)) * Math.sin(phi) * Math.sin(theta) ),
-          positionXstart = -((23 - radius) * Math.sin(phi) * Math.cos(theta)),
-          positionYstart = ((23 - radius) * Math.cos(phi)),
-          positionZstart = ((23 - radius) * Math.sin(phi) * Math.sin(theta) ),
-          position = `${positionX} ${positionY} ${positionZ}`,
-          positionStart = `${positionXstart} ${positionYstart} ${positionZstart}`;
+      phi = (90 - lat) * (Math.PI / 180),
+      theta = -((lng + 180) * (Math.PI / 180)),
+      positionX = -((28 - (index / 500)) * Math.sin(phi) * Math.cos(theta)),
+      positionY = ((28 - (index / 500)) * Math.cos(phi)),
+      positionZ = ((28 - (index / 500)) * Math.sin(phi) * Math.sin(theta)),
+      positionXstart = -((23 - radius) * Math.sin(phi) * Math.cos(theta)),
+      positionYstart = ((23 - radius) * Math.cos(phi)),
+      positionZstart = ((23 - radius) * Math.sin(phi) * Math.sin(theta)),
+      position = `${positionX} ${positionY} ${positionZ}`,
+      positionStart = `${positionXstart} ${positionYstart} ${positionZstart}`;
 
     // show a maximum number of 150 locations
-    if(this.state.locations.length >= 150){
+    if (this.state.locations.length >= 150) {
       this.setState(previousState => ({
-        locations: [...previousState.locations.filter((_, i) => i !== 0), {'position': position, 'positionStart': positionStart, 'index': index, 'color': color, 'type': type}],
+        locations: [
+          ...previousState.locations.filter((_, i) => i !== 0), {
+            'position': position,
+            'positionStart': positionStart,
+            'index': index,
+            'color': color,
+            'type': type
+          }
+        ],
         edits: previousState.edits + 1,
         editTitle: title
       }));
     } else {
       this.setState(previousState => ({
-        locations: [...previousState.locations, {'position': position, 'positionStart': positionStart, 'index': index, 'color': color, 'type': type}],
+        locations: [
+          ...previousState.locations, {
+            'position': position,
+            'positionStart': positionStart,
+            'index': index,
+            'color': color,
+            'type': type
+          }
+        ],
         edits: previousState.edits + 1,
         editTitle: title
       }));
     }
   }
   render() {
-    return (
-      <div className="scene-wrapper">
-        {!this.state.VRMode &&
-          <div>
-            <Content title={this.state.editTitle} />
-            <GitHub />
-          </div>}
-        {this.state.overlay}
-        <Scene>
-          <a-assets>
-            <img src={this.state.globeSrc} id="globe" />
-          </a-assets>
-          <Entity
-            id="vr-wikipedia-heatmap"
-            geometry="primitive: sphere; radius: 30; segmentsWidth: 16; segmentsHeight: 16;"
-            material="src: #globe; repeat: -1 1; side: back; fog: false;"
-            position="0 0 0">
-          </Entity>
-          <Locations locations={this.state.locations} />
-          <Lights />
-          <Entity camera="userHeight:0" wasd-controls="" look-controls="" />
-        </Scene>
-    </div>
-    );
+    return (<div className="scene-wrapper">
+      {
+        !this.state.VRMode && <div>
+            <Content title={this.state.editTitle}/>
+            <GitHub/>
+          </div>
+      }
+      {this.state.overlay}
+      <Scene>
+        <a-assets>
+          <img src={this.state.globeSrc} id="globe"/>
+        </a-assets>
+        <Entity id="vr-wikipedia-heatmap" geometry="primitive: sphere; radius: 30; segmentsWidth: 16; segmentsHeight: 16;" material="src: #globe; repeat: -1 1; side: back; fog: false;" position="0 0 0"></Entity>
+        <Locations locations={this.state.locations}/>
+        <Lights/>
+        <Entity camera="userHeight:0" wasd-controls="" look-controls=""/>
+      </Scene>
+    </div>);
   }
 }
 
-ReactDOM.render(<VRStream />, document.querySelector('#root'));
+ReactDOM.render(<VRStream/>, document.querySelector('#root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,81 @@
 const path = require('path'),
-webpack = require('webpack'),
-poststylus = require('poststylus'),
-HtmlWebpackPlugin = require('html-webpack-plugin');
+  webpack = require('webpack'),
+  poststylus = require('poststylus'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  dotenv = require('dotenv');
 
-module.exports = {
-  context: __dirname + "/app",
-  entry: ['babel-polyfill','./app.jsx'],
-  output: {
-    path: __dirname,
-    filename: "bundle.js"
-  },
-  resolveLoader: {
-    // Tell webpack to look for required files in node
-    modules: ['node_modules'],
-    extensions: ['.js', '.jsx', '.json', '.styl']
-  },
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
-  },
-  module: {
-    // npParse === aframe Bugfix for console warning
-    noParse: [
-      /node_modules\/aframe\/dist\/aframe-master.js/, // for aframe from NPM
-      /node_modules\/cannon\/build\/cannon.js/, // for aframe-extras from NPM
+module.exports = () => {
+  // call dotenv and it will return an Object with a parsed key
+  const env = dotenv.config().parsed;
+
+  // reduce it to a nice object, the same as before
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    context: __dirname + "/app",
+    entry: [
+      'babel-polyfill', './app.jsx'
     ],
-    loaders: [
-      {
-        test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'stage-0', 'react'],
+    output: {
+      path: __dirname,
+      filename: "bundle.js"
+    },
+    resolveLoader: {
+      // Tell webpack to look for required files in node
+      modules: ['node_modules'],
+      extensions: ['.js', '.jsx', '.json', '.styl']
+    },
+    node: {
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty'
+    },
+    module: {
+      // npParse === aframe Bugfix for console warning
+      noParse: [
+        /node_modules\/aframe\/dist\/aframe-master.js/, // for aframe from NPM
+        /node_modules\/cannon\/build\/cannon.js/, // for aframe-extras from NPM
+      ],
+      loaders: [
+        {
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015', 'stage-0', 'react']
+          }
+        }, {
+          test: /\.json$/,
+          loader: 'json-loader'
+        }, {
+          test: /\.styl$/,
+          loader: 'style-loader!css-loader!stylus-loader'
+        }, {
+          test: /\.(png|jpg|woff|woff2|eot|ttf|svg)(\?[a-z0-9=&.]+)?$/,
+          loader: 'url-loader?limit=100000'
         }
-      },{
-        test: /\.json$/,
-        loader: 'json-loader'
-      },{
-        test: /\.styl$/,
-        loader: 'style-loader!css-loader!stylus-loader',
-      },
-      { test: /\.(png|jpg|woff|woff2|eot|ttf|svg)(\?[a-z0-9=&.]+)?$/, loader: 'url-loader?limit=100000' }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin(envKeys),
+      new webpack.LoaderOptionsPlugin({
+        options: {
+          stylus: {
+            use: [poststylus(['autoprefixer'])]
+          }
+        }
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Wikipedia Edits VR Heatmap',
+        template: __dirname + '/app/index.html',
+        inject: 'body',
+        files: {
+          'css': [__dirname + '/app/style.styl']
+        }
+      })
     ]
-  },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        stylus: {
-          use: [poststylus([ 'autoprefixer' ])]
-        }
-      }
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Wikipedia Edits VR Heatmap',
-      template: __dirname + '/app/index.html',
-      inject: 'body',
-      files: {
-        'css': [ __dirname + '/app/style.styl']
-      }
-    })
-  ],
-};
+  };
+}
